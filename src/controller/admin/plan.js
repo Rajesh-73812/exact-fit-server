88144a649ba2670bd2a26ad2688502f458b2b7a9
rajesh <rajesh.swain@innoitlabs.com>
2025-11-24 11:47:11 +0530

    dated plansss
    fixes plan data structure# with '#' will be ignored, and an empty message aborts the commit.
    
    updated subscription plans logic

--- src/controller/admin/plan.js
+++ src/controller/admin/plan.js
@@ -136,7 +136,7 @@ const getPlanBySlug = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "plan fetched sucessfully",
-      data: plan
+      data: plan,
     });
     // return handleSuccessResponse(res, "plan fetched sucessfully", 200, plan);
   } catch (error) {
