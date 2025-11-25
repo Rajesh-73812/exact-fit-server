98a1ed6415f4d76fa948f6c0e471939137e22e20
rajesh <rajesh.swain@innoitlabs.com>
2025-11-25 09:48:45 +0530

    created usersubscription

--- src/controller/user/userSubscription.js
+++ src/controller/user/userSubscription.js
@@ -1,20 +1,39 @@
 const userSubscription = require("../../services/userSubscription");
-console.log(userSubscription);
+
 const createSubScriptionPlan = async (req, res) => {
-  // const { user_id, plan_id, property_type_id, start_date, end_date, price_total, payment_option,} = req.body;
-  // try {
-  //     const createPlan = await userSubscription.createSubscription();
-  //     return res.status(201).json({
-  //         success: true,
-  //         message: "subscription plan created successfully",
-  //     });
-  // } catch (error) {
-  //     console.error(error);
-  //     return res.status(500).json({
-  //         success: false,
-  //         error: error
-  //     });
-  // }
+  const {
+    plan_id,
+    property_type_id,
+    start_date,
+    end_date,
+    price_total,
+    payment_option,
+  } = req.body;
+  const created_by = req.user.id || {};
+
+  try {
+    const subscription = await userSubscription.createSubscription({
+      user_id: created_by,
+      plan_id,
+      property_type_id,
+      start_date,
+      end_date,
+      price_total,
+      payment_option,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Subscription plan created successfully",
+      subscription,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      error: error.message || "Something went wrong",
+    });
+  }
 };
 
 module.exports = {
