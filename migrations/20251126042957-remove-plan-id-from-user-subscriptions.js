f6c4fe863409e2050d7c548c167b572fa3bb9e4a
NARENDARKOREM <narendar.korem@innoitlabs.com>
2025-11-26 10:04:35 +0530

    modeidiieeeeeeeeee

--- migrations/20251126042957-remove-plan-id-from-user-subscriptions.js
+++ migrations/20251126042957-remove-plan-id-from-user-subscriptions.js
@@ -2,20 +2,13 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.removeColumn(
-      "user_subscriptions",
-      "plan_id"
-    );
+    await queryInterface.removeColumn("user_subscriptions", "plan_id");
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn(
-      "user_subscriptions",
-      "plan_id",
-      {
-        type: Sequelize.UUID,
-        allowNull: false,
-      }
-    );
+    await queryInterface.addColumn("user_subscriptions", "plan_id", {
+      type: Sequelize.UUID,
+      allowNull: false,
+    });
   },
 };
