ace2490e031989834b90e92fafa4172299cb7e2c
rajesh <rajesh.swain@innoitlabs.com>
2025-12-24 10:29:53 +0530

    added clear notification

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,13 +1,32 @@
+const { Op } = require("sequelize");
 const Notification = require("../models/notification");
 
 const getAll = async (user_id) => {
   return await Notification.findAll({
-    where: { user_id },
+    where: { user_id, is_read: true },
     attributes: ["title", "description", "createdAt"],
-    order: ["createdAt", "DESC"],
+    order: [["createdAt", "DESC"]],
   });
 };
 
+const clrearNotification = async (user_id, notification_ids) => {
+  try {
+    await Notification.update(
+      { is_read: true },
+      {
+        where: {
+          user_id,
+          id: { [Op.in]: notification_ids },
+        },
+      }
+    );
+    console.log(`${notification_ids.length} notifications marked as read`);
+  } catch (error) {
+    console.error("Error clearing notifications:", error);
+    throw new Error("Failed to clear notifications. Please try again later.");
+  }
+};
 module.exports = {
   getAll,
+  clrearNotification,
 };
